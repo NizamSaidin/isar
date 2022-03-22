d8b7f273831baf1b3f2cd8d649504d837ed434bd
Mohamad Nizam Saidin <nizam.saidin@phillips.my>
2022-03-22 22:20:42 +0800

    add dependencies

--- packages/isar_flutter_libs/tool/download_binaries.sh
+++ packages/isar_flutter_libs/tool/download_binaries.sh
@@ -4,10 +4,10 @@ core_version=`cat ../../CORE_VERSION`
 github="https://github.com/isar/isar-core/releases/download/${core_version:5}"
 
 
-curl "${github}/libisar_android_arm64.so" -o android/src/main/jniLibs/arm64-v8a/libisar.so --create-dirs -L
-curl "${github}/libisar_android_armv7.so" -o android/src/main/jniLibs/armeabi-v7a/libisar.so --create-dirs -L
-curl "${github}/libisar_android_x64.so" -o android/src/main/jniLibs/x86_64/libisar.so --create-dirs -L
-curl "${github}/libisar_ios.a" -o ios/libisar.a --create-dirs -L
-curl "${github}/libisar_macos_x64.dylib" -o macos/libisar.dylib --create-dirs -L
-curl "${github}/libisar_linux_x64.so" -o linux/libisar.so --create-dirs -L
-curl "${github}/isar_windows_x64.dll" -o windows/isar.dll --create-dirs -L
+cp libisar_android_x64.so android/src/main/jniLibs/arm64-v8a/libisar.so
+cp libisar_android_armv7.so android/src/main/jniLibs/armeabi-v7a/libisar.so
+cp libisar_android_x64.so android/src/main/jniLibs/x86_64/libisar.so
+cp libisar_ios.a ios/libisar.a
+cp libisar_macos_x64.dylib macos/libisar.dylib
+cp libisar_linux_x64.so linux/libisar.so
+cp isar_windows_x64.dll windows/isar.dll
